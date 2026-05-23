39e1feba82171afb5d6a17212dcbf6182d89bc11
DeathScytheZ <digital1808ta@gmail.com>
2026-05-24 00:11:41 +0100

    new feature create class

--- apps/backend/routes/systemAdminRoutes.js
+++ apps/backend/routes/systemAdminRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getRequestList, getUserList, createChild, createParent, getChildren } = require("../controllers/systemAdminController");
+const { getRequestList, getUserList, createChild, createParent, getChildren, createClass, getAvailableClasses, getClasses } = require("../controllers/systemAdminController");
 
 router.get('/request', getRequestList);
 router.get('/users', getUserList);
 router.post('/create-child', createChild);
 router.post('/create-parent', createParent);
 router.get('/children', getChildren);
+router.post('/create-class', createClass);
+router.get('/available-classes/:type', getAvailableClasses);
+router.get('/classes', getClasses);
 
 module.exports = router;
